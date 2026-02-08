97879d19af0199f96d483e6cfb00f2ad033f455d
efim <efimmanevich@gmail.com>
2026-02-08 20:58:15 +0300

    wine2 spere

--- CadQuery/wineglass2.py
+++ CadQuery/wineglass2.py
@@ -4,10 +4,10 @@ import math
 import numpy as np
 
 def fnsteam(x):
-    return 0.9*x*x
+    return 0.8*x*x
 
 def inverse_fnsteam(y):
-    return math.sqrt(y/0.9)
+    return math.sqrt(y/0.8)
 
 
 
@@ -20,10 +20,10 @@ def fn_r8(r):
 
 def wine2():
     r0 = 0.9  #diameter at the thinnest part of the leg
-    h0 = 3    #height of the thinnest part of the leg  
+    h0 = 3.5    #height of the thinnest part of the leg  
     r1 = r0 + fnsteam(h0) - 1.5  #We calculate the diameter of the base of the leg
-    r2 = 0.5*r1 #diameter of the ball at the middle of the leg
-    h2 = r2     #height of the prism for the ball in the middle
+    r2 = 0.45*r1 #diameter of the ball at the middle of the leg
+    h2 = 1.6*r2     #height of the prism for the ball in the middle
 
     r8 = fn_r8(r2/2)  #We calculate the smaller radius in an eight-pointed star
     r8a = r8/math.cos(math.pi/8) 
@@ -37,15 +37,15 @@ def wine2():
 
     rbox = 2*fnsteam(h0) + 2
 
-    h9 = 0.9 * (h0 - h8) #height of the upper section of the leg
+    h9 = 1.0 * (h0 - h8) #height of the upper section of the leg
     r9 = fnsteam(h9) + r0 #
     w9 = 0.3
 
     bowl_h = 1.2*h0
-    bowl_r1 = 1.5*r1/2
+    bowl_r1 = 1.7*r1/2
     bowl_r2 = bowl_r1 * 0.3
     
-    bowl_w = 0.3
+    bowl_w = 0.2
 
     
     # ellipsoid
@@ -94,13 +94,31 @@ def wine2():
     
     sharpener2 = cq.Workplane("XY").box(rbox/2, rbox/2, 3*h0, centered=(True, True, False)).cut(stem)
     sharpener2 = sharpener2.translate((0, 0, h28 - h8))
-    sp = sp.union(sp.rotate((0, 0, 0), (0, 1, 0), 180)).edges("|Z").fillet(0.01*r2)
+    sp = sp.cut(sharpener2)
+    
+    #calculate bevel
+    a = np.array([r2/2, 0, 0])
+    b = np.array([r8 * math.cos(math.pi/8), r8 * math.sin(math.pi/8), h28])
+    c = np.array([r8 * math.cos(math.pi/8), -r8 * math.sin(math.pi/8), h28])
+    v1 = b - a
+    v2 = c - a
+    norm = np.cross(v1, v2)
+    norm = norm/np.linalg.norm(norm)
+    m = (a + b + c)/3
+    d = np.linalg.norm(v1+v2)
+    pl = cq.Plane(origin=(m[0], m[1], m[2]), normal = (norm[0], norm[1], norm[2]))
+    sharpener40 = cq.Workplane(pl).box(d, d, d, centered=(True, True, False))
+    for i in range(8):
+        ct = sharpener40.rotate((0, 0, 0), (0, 0, 1), i*45)
+        sp = sp.cut(ct)
 
-    sp = sp.cut(sharpener2)
-    sharpener2 = sharpener2.rotate((0, 0, 0), (0, 1, 0), 180)
-    sp = sp.cut(sharpener2)
+    sp = sp.union(sp.rotate((0, 0, 0), (0, 1, 0), 180))#.edges("|Z").fillet(0.01*r2)
+
+
+
     sp = sp.translate((0, 0, h0 + h3))
     comp = cq.Compound.makeCompound([sp.val(), stem_bot.val(), stem_top.val(), bowl.val()])
+    #comp = cq.Compound.makeCompound([sp.val()])
     ass = cq.Assembly()
     ass.add(comp)
     return ass
