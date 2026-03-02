92c9d5c783e6772999424644583b3dc7bf15bc6c
efim <efimmanevich@gmail.com>
2026-03-02 21:13:53 +0300

    vase2 wave

--- CadQuery/vase2.py
+++ CadQuery/vase2.py
@@ -17,12 +17,43 @@ def Curve3D(fn, path, r, start):
         .sweep(path, isFrenet=True) 
     )
     return result   
- 
+
+def wave2 (n, r = 1, a = math.pi/4):
+    def fn(t, n, r, d):
+        r1 = r + d*r*math.sin(t*n)
+        return (r1*math.cos(t), r1*math.sin(t))
+    
+    nn = 200
+    d = 0.2
+    h = r/math.tan(a)
+    p = [fn (math.tau*i/nn, n, r, 0.1)  for i in range(nn)]
+    res = cq.Workplane("XY").spline(p).close().workplane(offset=h).circle(0.01).loft()
+    w = 2.0 * (r + d*r) + 0.5
+    box = cq.Workplane("XY").box(w, w, h + 0.5, centered=(True, True, False))
+    box = box.cut(res)
+    #show(box)
+    return box
+
+
+
+def wave(n, r = 1) -> cq.Solid:
+    alf = math.pi/n
+    h = r/math.tan(alf/2) 
+    cn = cq.Solid.makeCone(r, 0, h).rotate((0, 0, 0), (0, 1, 0), -90).translate((h, 0, 0))
+    unioncn = [cn.rotate((0, 0, 0), (0, 0, 1), 360/n*i) for i in range(n)]
+    cutn = [cn.rotate((0, 0, 0), (0, 0, 1), 360/n*i+180/n) for i in range(n)]
+    ucomp = cq.Compound.makeCompound(unioncn)
+    ccomp = cq.Compound.makeCompound(cutn)
+    res = cq.Solid.makeCylinder(h, 2*r)
+    res = res.fuse(ucomp)
+    res = res.cut(ccomp)
+    return res
+
 def vase2():
 
     
 
-    x_krita = [625, 575, 550, 537.5, 400, 300, 225, 150, 75]  # coords from krita project
+    x_krita = [605, 575, 550, 537.5, 400, 300, 225, 150, 75]  # coords from krita project
     y_krita = [400, 400,  375, 387.5, 462.5, 462.5, 400, 387.5, 425] # coords from krita project
     y0 = 312.5  #coords from krita project
 
@@ -41,8 +72,8 @@ def vase2():
     nn = 13  # count segments
     nna = 5  # count twist segments
     r = 100  # bounded radius 
-    w = 1.2  # width
-    r1 = w*0.5  # radius diamond
+    w = 1.  # width
+    r1 = w*0.5  # radius diamond line
     sm = 1.15   #
     
 
@@ -77,9 +108,13 @@ def vase2():
     line2 = line2.cut(box.translate((0, 0, h)))
     line2 = line2.cut(box.translate((0, 0, -10)))
 
+
+    wv = wave2(nn, y_coords[-1], a=math.pi/3)
+    wv = wv.translate((0, 0, x_coords[-1]- 1.35)).rotate((0, 0, 0), (0, 0, 1), -90/nn)
+    
     res = res.faces(">Z").shell(-w)
-    res = res.edges(">Z").fillet(0.4*w)
-    res = res.edges("<Z").fillet(w)
+    res = res.cut(wv)
+    res = res.edges().fillet(0.4*w)
     
     lines = [res.val()]
     
@@ -93,6 +128,8 @@ def vase2():
     ass = cq.Assembly()
     ass.add(bwcomp)
     return ass
+
+
 
 res = vase2()    
 res.save("./stl/vase2.glb")
