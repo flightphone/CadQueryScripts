97e0eebdad352b49282f54e9d9a38a33f532885e
efim <efimmanevich@gmail.com>
2026-03-02 08:21:33 +0300

    vase2

--- CadQuery/vase2.py
+++ CadQuery/vase2.py
@@ -22,8 +22,8 @@ def vase2():
 
     
 
-    x_krita = [537.5, 400, 300, 225, 150, 75]
-    y_krita = [387.5, 462.5, 462.5, 400, 387.5, 425]
+    x_krita = [ 550, 537.5, 400, 300, 225, 150, 75]
+    y_krita = [ 375, 387.5, 462.5, 462.5, 400, 387.5, 425]
     y0 = 312.5
 
     x_coords = [(x_krita[0] - x)/10 for x in x_krita]
@@ -60,26 +60,34 @@ def vase2():
         intersections = res.val().intersect(line)
         if intersections:
             return intersections.Vertices()[0].X, intersections.Vertices()[0].Y, intersections.Vertices()[0].Z    
-        
-    path1 = cq.Workplane("XY").parametricCurve(helix1,  start=0., stop = h)
+    
+    box = cq.Workplane("XY").box(100, 100, 10, centered=(True, True, False))    
+    path1 = cq.Workplane("XY").parametricCurve(helix1,  start=0.01, stop = 0.99*h)
     line1 = Curve3D(helix1, path1, r1, 0)
+    line1 = line1.cut(box.translate((0, 0, x_coords[-1])))
+    line1 = line1.cut(box.translate((0, 0, -10)))
 
-    path2 = cq.Workplane("XY").parametricCurve(helix2,  start=0., stop = h)
+    path2 = cq.Workplane("XY").parametricCurve(helix2,  start=0.01, stop = 0.99* h)
     line2 = Curve3D(helix2, path2, r1, 0)
+    line2 = line2.cut(box.translate((0, 0, x_coords[-1])))
+    line2 = line2.cut(box.translate((0, 0, -10)))
 
-    lines = []
+    res = res.faces(">Z").shell(-w)
+    res = res.edges(">Z").fillet(0.4*w)
+    
+    lines = [res.val()]
+    
     for i in range(nn):
         li1 = line1.rotate((0, 0, 0), (0, 0, 1), i / nn * 360)
         lines.append(li1.val())
         li2 = line2.rotate((0, 0, 0), (0, 0, 1), i / nn * 360)
         lines.append(li2.val())
-
+    
     bwcomp = cq.Compound.makeCompound(lines)   
+    ass = cq.Assembly()
+    ass.add(bwcomp)
+    return ass
 
-    res = res.faces(">Z").shell(-w)
-    res = res.fillet(0.3*w) 
-
-    show(res, bwcomp)
-    #print(helix1(0.5*h))
-
-vase2()
+res = vase2()    
+res.save("./stl/vase2.glb")
+show(res)
